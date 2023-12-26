a2653971dceadb890ecaffce987b6b66eea0b3a3
Graham <blundell.gp@gmail.com>
2023-12-25 22:06:59 -0800

    added content on gallery and about, sized and prepared

--- wildwood/src/pages/Gallery.js
+++ wildwood/src/pages/Gallery.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Gallery.css'
+import plane from '../assets/plane.avif'
 
 const Gallery = () => {
   return (
-    <div>
-      <h1>gallery</h1>
+    <div className='gallery-div-wrap'>
+      <div className='job-1'>
+        <img className='job-2' src={plane} />
+      </div>
+      <div className='job-1'>
+      <img className='job-2' src={plane} />
+      </div>
+      <div className='job-1'>
+      <img className='job-2' src={plane} />
+      </div>
+      <div className='job-1'>
+      <img className='job-2' src={plane} />
+      </div>
+      <div className='job-1'>
+      <img className='job-2' src={plane} />
+      </div>
+      <div className='job-1'>
+      <img className='job-2' src={plane} />
+      </div>
     </div>
   )
 }
